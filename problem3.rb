b00ff6a15a28b921839fd4c84b296164942ef5b7
Hilmi Abdul Hanif <hilmi.abdulhanif@gmail.com>
2017-12-04 22:41:26 +0800

    Add solution to Problem 3

---
+++ problem3.rb
@@ -0,0 +1,21 @@
+# What is the largest prime factor of 600851475143
+
+require 'prime'
+
+prime_factors   = []
+original_number = 600851475143
+number          = original_number
+
+until number.prime?
+    Prime.each(number) do |prime|
+        if number % prime == 0
+            prime_factors << prime
+            number = number/prime
+            break
+        end
+    end
+end
+
+puts "The largest prime factor of #{original_number} is #{prime_factors.last}."
+
+
